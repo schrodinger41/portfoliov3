8dac3d10e994c23cf23d4984ffcc992e542257ec
jhet <jhetdizon41@gmail.com>
2024-07-20 22:58:29 +0800

    ...

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -14,23 +14,19 @@ const Home = () => {
   ];
   const [index, setIndex] = useState(0);
   const [typingIndex, setTypingIndex] = useState(0);
-  const [typingDirection, setTypingDirection] = useState(1); // 1 for typing, -1 for erasing
-
+  const [typingDirection, setTypingDirection] = useState(1);
   useEffect(() => {
     const currentStatus = statuses[index];
     const length = currentStatus.length;
 
-    // Determine whether to type or erase
     if (typingDirection === 1) {
       if (typingIndex < length) {
-        // Typing
         const timeoutId = setTimeout(() => {
           setStatus((prevStatus) => prevStatus + currentStatus[typingIndex]);
           setTypingIndex((prevTypingIndex) => prevTypingIndex + 1);
         }, 100);
         return () => clearTimeout(timeoutId);
       } else {
-        // Finished typing, wait for 2 seconds then start erasing
         const timeoutId = setTimeout(() => {
           setTypingDirection(-1);
         }, 2000);
@@ -38,22 +34,19 @@ const Home = () => {
       }
     } else {
       if (typingIndex > 0) {
-        // Erasing
         const timeoutId = setTimeout(() => {
-          setStatus((prevStatus) => prevStatus.slice(0, -1)); // Remove last character
+          setStatus((prevStatus) => prevStatus.slice(0, -1));
           setTypingIndex((prevTypingIndex) => prevTypingIndex - 1);
         }, 60);
         return () => clearTimeout(timeoutId);
       } else {
-        // Finished erasing, move to the next status
         const nextIndex = (index + 1) % statuses.length;
-        // Set typingIndex to 0 immediately to start typing next status
         setTypingIndex(0);
         const timeoutId = setTimeout(() => {
           setStatus("");
           setIndex(nextIndex);
           setTypingDirection(1);
-        }, 0); // No delay for typing
+        }, 0);
         return () => clearTimeout(timeoutId);
       }
     }
